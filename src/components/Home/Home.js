87bdfee481ad7ea2fd1b60816de5eb7fd48c0d23
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-04-05 01:20:27 +0600

    Connect

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='page-bg'>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Home;
